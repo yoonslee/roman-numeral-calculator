ab723e3b79227951eaad0a81c9106731e4c8b705
Yoon Lee <himynameisyoon@gmail.com>
2019-05-10 18:29:04 -0700

    add styled button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Button = styled.button``;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Button</Button>
     </div>
   );
 }
