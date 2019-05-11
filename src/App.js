34c2cd6675d48a97a5ddc79bedce359faf66af63
Yoon Lee <himynameisyoon@gmail.com>
2019-05-10 19:26:34 -0700

    add logic for clicking equals button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styled from "@emotion/styled";
 
 import NUMERALS from "./data/NUMERALS";
+import OPERATIONS, { OPERATIONS_KEYS } from "./data/OPERATIONS";
 
 const Button = styled.button``;
 const Input = styled.input``;
@@ -9,22 +10,59 @@ const Input = styled.input``;
 function App() {
   const [first, setFirst] = useState("");
   const [second, setSecond] = useState("");
-  const [inputMode, setInputMode] = useState(0);
+  const [operationMode, setOperationMode] = useState();
 
   return (
     <div className="App">
-      <Input value={first} />
-      <Input value={second} />
+      {`FIRST: ${first} / SECOND: ${second} / operationMode: ${operationMode}`}
+
+      {!operationMode || operationMode === OPERATIONS_KEYS.EQUALS ? (
+        <Input value={first} readOnly />
+      ) : (
+        <Input value={second} readOnly />
+      )}
 
       {Object.keys(NUMERALS).map(numeral => (
         <Button
           key={numeral}
           onClick={() => {
             console.log(numeral, NUMERALS[numeral].value);
-            return setFirst(`${first}${numeral}`);
+
+            if (!operationMode) return setFirst(`${first}${numeral}`);
+            return setSecond(`${second}${numeral}`);
           }}
         >
           {numeral}
+        </Button>
+      ))}
+
+      {Object.keys(OPERATIONS).map(opKey => (
+        <Button
+          key={opKey}
+          onClick={() => {
+            console.log(opKey, OPERATIONS[opKey].symbol);
+
+            if (opKey === OPERATIONS_KEYS.EQUALS) {
+              let isThisEqualClickValid = true;
+
+              // check to see if valid: current `operationMode` is not equals but is something else AND two inputs received
+              if (!operationMode || operationMode === OPERATIONS_KEYS.EQUALS)
+                isThisEqualClickValid = false;
+
+              if (!first || !second) isThisEqualClickValid = false;
+
+              // we should not set `operationMode` to EQUALS if the equal click is not valid or output any result
+              if (!isThisEqualClickValid) return;
+
+              // if valid, calculate result and set it to first input, remove second input for subsequent calculation
+              setFirst(`RESULT`);
+              setSecond("");
+            }
+
+            return setOperationMode(opKey);
+          }}
+        >
+          {OPERATIONS[opKey].symbol}
         </Button>
       ))}
     </div>
---
+++ src/data/OPERATIONS.js
@@ -0,0 +1,25 @@
+export const OPERATIONS_KEYS = {
+  ADD: "ADD",
+  SUBTRACT: "SUBTRACT",
+  MULTIPLY: "MULTIPLY",
+  DIVIDE: "DIVIDE",
+  EQUALS: "EQUALS"
+};
+
+export default {
+  [OPERATIONS_KEYS.ADD]: {
+    symbol: "+"
+  },
+  [OPERATIONS_KEYS.SUBTRACT]: {
+    symbol: "-"
+  },
+  [OPERATIONS_KEYS.MULTIPLY]: {
+    symbol: "*"
+  },
+  [OPERATIONS_KEYS.DIVIDE]: {
+    symbol: "/"
+  },
+  [OPERATIONS_KEYS.EQUALS]: {
+    symbol: "="
+  }
+};
