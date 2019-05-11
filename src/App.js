cefad2d75cab2d91d3c7236c88a98107dae74538
Yoon Lee <himynameisyoon@gmail.com>
2019-05-10 18:54:55 -0700

    begin to add state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 
+import NUMERALS from "./data/NUMERALS";
+
 const Button = styled.button``;
+const Input = styled.input``;
 
 function App() {
+  const [first, setFirst] = useState("");
+  const [second, setSecond] = useState("");
+  const [inputMode, setInputMode] = useState(0);
+
   return (
     <div className="App">
-      <Button>Button</Button>
+      <Input value={first} />
+      <Input value={second} />
+
+      {Object.keys(NUMERALS).map(numeral => (
+        <Button
+          key={numeral}
+          onClick={() => {
+            console.log(numeral, NUMERALS[numeral].value);
+            return setFirst(`${first}${numeral}`);
+          }}
+        >
+          {numeral}
+        </Button>
+      ))}
     </div>
   );
 }
