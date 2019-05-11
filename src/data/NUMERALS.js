50bf2ef748f8926de68d16ea37f52a318cc9e354
Yoon Lee <himynameisyoon@gmail.com>
2019-05-10 18:54:25 -0700

    make numerals constants

---
+++ src/data/NUMERALS.js
@@ -0,0 +1,26 @@
+export default {
+  I: {
+    value: 1
+  },
+  V: {
+    value: 5
+  },
+  X: {
+    value: 10
+  },
+  L: {
+    value: 50
+  },
+  C: {
+    value: 100
+  },
+  D: {
+    value: 500
+  },
+  M: {
+    value: 1000
+  }
+  // N: {
+  //   value: 0
+  // }
+};
