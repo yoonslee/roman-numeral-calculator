6a00005b47e99da95c1833b06f591b015f5082da
Yoon Lee <himynameisyoon@gmail.com>
2019-06-18 20:51:28 -0700

    add tests based on different states in App's input value

--- src/__tests__/components/ClearButton.js
+++ src/__tests__/components/ClearButton.js
@@ -15,9 +15,30 @@ describe("ClearButton component", () => {
     expect(tree).toMatchSnapshot();
   });
 
-  // testing 9 different combinations of empty/non-empty (1) first numeral value, (2) second numeral value, and (3) operation mode
+  // testing >9 different combinations of empty/non-empty (1) first numeral value, (2) second numeral value, and (3) operation mode, ignoring impossible cases
   describe("handleClear clears text", () => {
     afterEach(cleanup);
+
+    test("empty first value, empty second value, empty operation mode", () => {
+      const { getByTestId, getByText } = render(<App />);
+
+      // grab input
+      const input = getByTestId("input");
+
+      // get value of input
+      const valueBefore = input.value;
+
+      // click the handleClear button
+      fireEvent.click(getByText(/AC/));
+
+      // expect there to be an empty value in input
+      const valueAfter = input.value;
+
+      // empty string because anticipating second numeral
+      expect(valueBefore).toBe("");
+      // first numeral shows, waiting for operation selection
+      expect(valueAfter).toBe("");
+    });
 
     test("non-empty first value, empty second value, non-empty operation mode", () => {
       const initialFirst = "X";
@@ -65,6 +86,57 @@ describe("ClearButton component", () => {
       expect(valueBefore).toBe(initialFirst);
       // empty string, waiting for first numeral
       expect(valueAfter).toBe("");
+    });
+
+    test("non-empty first value, non-empty second value, operation mode", () => {
+      const initialFirst = "X";
+      const initialSecond = "XX";
+
+      const { getByTestId, getByText } = render(
+        <App first={initialFirst} second={initialSecond} operationMode="ADD" />
+      );
+
+      // grab input
+      const input = getByTestId("input");
+
+      // get value of input
+      const valueBefore = input.value;
+
+      // click the handleClear button
+      fireEvent.click(getByText(/AC/));
+
+      // expect there to be an empty value in input
+      const valueAfter = input.value;
+
+      // second numeral shows, waiting for equals operation
+      expect(valueBefore).toBe(initialSecond);
+      // empty string, waiting for second numeral
+      expect(valueAfter).toBe("");
+    });
+
+    test("non-empty first value, empty second value, equals operation mode", () => {
+      const initialFirst = "X";
+
+      const { getByTestId, getByText } = render(
+        <App first={initialFirst} operationMode="EQUALS" />
+      );
+
+      // grab input
+      const input = getByTestId("input");
+
+      // get value of input
+      const valueBefore = input.value;
+
+      // click the handleClear button
+      fireEvent.click(getByText(/AC/));
+
+      // expect there to be an empty value in input
+      const valueAfter = input.value;
+
+      // first numeral shows, waiting for next operation selection
+      expect(valueBefore).toBe(initialFirst);
+      // after the handleClear, first numeral still shows but is editable, allowing user to input append to the first numeral
+      expect(valueAfter).toBe(initialFirst);
     });
   });
 });
